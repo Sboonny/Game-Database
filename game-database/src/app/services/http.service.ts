0a1ba91388220a9f2c02fce181321c79591987f8
Sboonny <88248797+Sboonny@users.noreply.github.com>
2022-01-10 17:30:23 +0200

    added images for platform and games

--- game-database/src/app/services/http.service.ts
+++ game-database/src/app/services/http.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { stringify } from 'querystring';
+// import { stringify } from 'querystring';
 import { Observable } from 'rxjs';
 import { environment as env } from 'src/environments/environment';
 import { APIResponse, Game } from '../models';
